79028ce834a5f9ac5e57d16450ae0213fa340abf
AshleyNicole1319 <a-warford96@comcast.net>
2022-09-23 11:21:45 -0400

    connect to database

---
+++ server.js
@@ -0,0 +1,20 @@
+const inquirer = require('inquirer');
+const mysql = require
+
+
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        port:'3001',
+        user:'root',
+        password: '7141',
+        database: 'employee_db'
+    }
+
+);
+db.connect(function(err){
+    if(err) throw err;
+
+
+    console.log("Connected to employee_db database.");
+})
